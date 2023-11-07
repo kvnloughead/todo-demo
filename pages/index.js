2c85e100061da17e411ed9c7af3f6384cdfa6333
Kevin Loughead <kvnloughead@gmail.com>
2023-11-07 16:25:44 -0500

    feat: submission and closing for modal

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,42 @@ const initialTodos = [
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoPopup = document.querySelector(".popup");
+const addTodoForm = addTodoPopup.querySelector(".popup__form");
+const addTodoCloseBtn = addTodoPopup.querySelector(".popup__close");
+const todoTemplate = document.querySelector("#todo-template");
+const todosList = document.querySelector(".todos__list");
 
 const openModal = (modal) => {
   modal.classList.add("popup_visible");
+};
+
+const closeModal = (modal) => {
+  modal.classList.remove("popup_visible");
+};
+
+const generateTodo = (data) => {
+  const todoElement = todoTemplate.content.firstElementChild.cloneNode(true);
+  const todoNameEl = todoElement.querySelector(".todo__name");
+  todoNameEl.textContent = data.name;
+  return todoElement;
 };
 
 addTodoButton.addEventListener("click", () => {
   openModal(addTodoPopup);
 });
 
-initialTodos.forEach((item) => {});
+addTodoCloseBtn.addEventListener("click", () => {
+  closeModal(addTodoPopup);
+});
+
+addTodoForm.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const todo = generateTodo({ name: evt.target["todo-name"].value });
+  todosList.append(todo);
+  closeModal(addTodoPopup);
+});
+
+initialTodos.forEach((item) => {
+  const todo = generateTodo(item);
+  todosList.append(todo);
+});
