1fa44d883a804646620cba24db5b4ec9738b8d7d
Kevin Loughead <kvnloughead@gmail.com>
2023-11-07 15:40:16 -0500

    Add basic html and styles

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,7 @@
-import Todo from "../components/Todo.js";
-import TodoCounter from "../components/TodoCounter.js";
-import Section from "../components/Section.js";
-import { initialTodos } from "../utils/constants.js";
+const initialTodos = [
+  { id: 1, name: "Clone starting repo", completed: true },
+  { id: 2, name: "Read instructions thoroughly", completed: false },
+  { id: 3, name: "Complete project", completed: false },
+];
 
-// Project 8
-const todoCounter = new TodoCounter(initialTodos, ".todos__counter");
-const handleChecked = (checked) => {
-  todoCounter.updateCompleted(checked);
-};
-
-const handleDelete = (completed) => {
-  todoCounter.updateTotal({ deleted: true, completed });
-};
-
-// const todosList = document.querySelector(".todos__list");
-const todosList = new Section(initialTodos, ".todos__list", (item) => {
-  const todo = new Todo(item, "#todo-template", handleChecked, handleDelete);
-  const todoElement = todo.getView();
-  todosList.addItem(todoElement);
-});
-todosList.renderItems();
-
-// Implementation prior to adding Section class in Project 8
-// initialTodos.forEach((item) => {
-//   const todo = new Todo(item, "#todo-template", handleChecked, handleDelete);
-//   const todoElement = todo.getView();
-//   todosList.append(todoElement);
-// });
+initialTodos.forEach((item) => {});
--- utils/constants.js
+++
@@ -1,5 +0,0 @@
-export const initialTodos = [
-  { id: 1, name: "Clone starting repo", completed: true },
-  { id: 2, name: "Read instructions thoroughly", completed: false },
-  { id: 3, name: "Complete project", completed: false },
-];
