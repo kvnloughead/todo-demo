affb0459ed27bfee99fe1cfd8aa1dcf92ede89e2
Kevin Loughead <kvnloughead@gmail.com>
2023-11-08 15:33:55 -0500

    feat: add Section class

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
 import FormValidator from "../components/FormValidator.js";
 import Todo from "../components/Todo.js";
 import TodoCounter from "../components/TodoCounter.js";
+import Section from "../components/Section.js";
 import PopupWithForm from "../components/PopupWithForm.js";
 import { initialTodos, formConfig } from "../utils/constants.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
 const addTodoForm = document.querySelector("#add-todo-form");
-const todosList = document.querySelector(".todos__list");
-
-const addTodoFormValidator = new FormValidator(formConfig, addTodoForm);
-addTodoFormValidator.enableValidation();
 
 const todoCounter = new TodoCounter(initialTodos, ".todos__counter");
+
 const handleChecked = (checked) => {
   todoCounter.updateCompleted(checked);
 };
@@ -26,13 +24,22 @@ const generateTodo = (data) => {
   return todoElement;
 };
 
+const todosList = new Section(initialTodos, ".todos__list", (item) => {
+  const todo = generateTodo(item);
+  todosList.addItem(todo);
+});
+todosList.renderItems();
+
+const addTodoFormValidator = new FormValidator(formConfig, addTodoForm);
+addTodoFormValidator.enableValidation();
+
 addTodoButton.addEventListener("click", () => {
   addTodoPopup.open();
 });
 
 const handleFormSubmit = (data) => {
   const todo = generateTodo(data);
-  todosList.append(todo);
+  todosList.addItem(todo);
   addTodoPopup.close();
 
   // Increment counter
@@ -48,8 +55,3 @@ const addTodoPopup = new PopupWithForm({
   handleFormSubmit,
 });
 addTodoPopup.setEventListeners();
-
-initialTodos.forEach((item) => {
-  const todo = generateTodo(item);
-  todosList.append(todo);
-});
